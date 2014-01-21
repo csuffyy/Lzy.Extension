69b56054690adc34abacf2e2bcb81d96ca024dd2
csuffyy <csu-lzy@163.com>
2014-01-21 14:30:16 +0800

    增加了通过反射获取属性信息的方法

--- Lzy.Extension/ObjectExtension.cs
+++ Lzy.Extension/ObjectExtension.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace Lzy.Extension
 {
@@ -27,6 +30,46 @@ namespace Lzy.Extension
         public static T As<T>(this object obj)
         {
             return (T)(object)obj;
+        }
+        
+        /// <summary>
+        /// 根据属性名称获取属性的值
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="t">初始化以后的实例</param>
+        /// <param name="propertyName">属性的名称</param>
+        /// <returns>属性的值</returns>
+        public static object GetPropertyValue<T>(this T t, string propertyName)
+        {
+            object obj;
+            try
+            {
+                obj = typeof(T).GetProperty(propertyName).GetValue(t, null);
+            }
+            catch (AmbiguousMatchException)
+            {
+                throw new AmbiguousMatchException("属性名称错误");
+            }
+            catch (NullReferenceException)
+            {
+                throw new NullReferenceException("属性名称错误");                
+            }
+            catch (Exception)
+            {
+                throw new Exception("属性名称错误");
+            }
+            return obj;
+        }
+
+        /// <summary>
+        /// 获取所有属性的名称
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="t">实例</param>
+        /// <returns>属性名称</returns>
+        public static IEnumerable<string> GetPropertyNames<T>(this T t)
+        {
+            return typeof(T).GetProperties().ToList().Select(p => p.Name);
         }
     }
 }
